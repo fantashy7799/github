cc5d5792d4d447fee8f4fa8edba5f280bc9279d8
Nguyễn Uy Định <nguyenuydinh@MacBook-Pro-cua-Nguyen.local>
2023-03-23 15:21:10 +0700

    register

--- github/src/App.js
+++ github/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a>abc</a>
+        <a>register</a>
       </header>
     </div>
   );
